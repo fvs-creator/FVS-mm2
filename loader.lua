eac72c6283d2221595207a7ae86fd4da96c61470
fvs-creator <vincejoshuatiktok@gmail.com>
2026-02-27 13:51:09 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,1 @@
-ID="cf45c4cb-0354-43d9-828e-333c3f2c4ba6";
-RUN_AFTER=loadstring(game:HttpGet("https://api.overdrivehub.xyz/v1/auth"));
-loadstring(game:HttpGet("http://72.56.90.233:3910/cdn/loader.luau"))();
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImNmNDVjNGNiLTAzNTQtNDNkOS04MjhlLTMzM2MzZjJjNGJhNiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL2FwaS5vdmVyZHJpdmVodWIueHl6L3YxL2F1dGgifX0"))()
