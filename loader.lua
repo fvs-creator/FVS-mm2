2e64c0e5dfdb135b8ada1dd68cb7c8a56e9ca74c
fvs-creator <vincejoshuatiktok@gmail.com>
2026-05-16 12:30:52 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImNmNDVjNGNiLTAzNTQtNDNkOS04MjhlLTMzM2MzZjJjNGJhNiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/3gycsOfqgN/0"))()
